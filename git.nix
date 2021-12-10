60022a0cb827ed6b4989f44379217516e25ba67e
Satsuki Akiba <anosatsuk124@gmail.com>
2021-12-10 09:11:56 +0900

    remove my contancts

--- git.nix
+++ git.nix
@@ -1,7 +1,7 @@
 {
   programs.git = {
     enable = true;
-    userName = "Satsuki Akiba";
-    userEmail = "anosatsuk124@gmail.com";
+    userName = "Foo Bar";
+    userEmail = "example@example.com";
   };
 }
